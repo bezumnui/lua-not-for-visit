51ee7093bb4e5d99241e8b58ece95657614f77f1
bezumnui <57602929+bezumnui@users.noreply.github.com>
2022-10-27 19:33:10 +0400

    Update load.lua

--- load.lua
+++ load.lua
@@ -1,5 +1,5 @@
 --
-local http = require 'socket.http'
+local http = require("socket.http")
 local res, code, headers, status = http.request('https://raw.githubusercontent.com/bezumnui/lua-not-for-visit/main/test.lua')
 load(res):read"*a"()
 -- 
