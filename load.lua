83f4ec8b745f79100a3a788841abd85bca7cb018
bezumnui <57602929+bezumnui@users.noreply.github.com>
2022-10-27 19:36:03 +0400

    Update load.lua

--- load.lua
+++ load.lua
@@ -1,5 +1,13 @@
 --
-local http = require("socket.http")
-local res, code, headers, status = http.request('https://raw.githubusercontent.com/bezumnui/lua-not-for-visit/main/test.lua')
-load(res):read"*a"()
+(function(u,f)
+    -- get a temp file name, Windows prefixes those with a \, so remove that
+    f=f or os.tmpname():gsub('^\\','')
+    -- run curl, make it output into our temp file
+    os.execute(('curl -s "%s" -o "%s"'):format(u,f))
+    -- load/run temp file
+    loadfile(f)()
+    os.remove(f)
+end)("https://raw.githubusercontent.com/bezumnui/lua-not-for-visit/main/test.lua");
+--local res, code, headers, status = http.request('')
+--load(res):read"*a"()
 -- 
